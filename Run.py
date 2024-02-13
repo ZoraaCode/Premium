eafe5e88ccdf3a3898197bbefcc662dcedd03971
ZoraaCode <155619038+ZoraaCode@users.noreply.github.com>
2024-02-14 01:45:36 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -2,28 +2,23 @@ import os, sys
 
 try:
     import requests
-except ImportError as e:
-    print('\n [\x1b[1;91m!\x1b[0m]  {str(e).title()}')
+except ImportError:
     os.system('pip install requests')
 try:
     import rich
-except ImportError as e:
-    print('\n [\x1b[1;91m!\x1b[0m]  {str(e).title()}')
+except ImportError:
     os.system('pip install rich')
 try:
     import bs4
-except ImportError as e:
-    print('\n [\x1b[1;91m!\x1b[0m]  {str(e).title()}')
+except ImportError:
     os.system('pip install bs4')
 try:
     import Cryptodome
-except ImportError as e:
-    print('\n [\x1b[1;91m!\x1b[0m]  {str(e).title()}')
+except ImportError:
     os.system('pip install pycryptodomex')
 try:
     import nacl
-except ImportError as e:
-    print('\n [\x1b[1;91m!\x1b[0m]  {str(e).title()}')
+except ImportError:
     os.system('pkg install clang python libffi openssl libsodium && SODIUM_INSTALL=system pip install pynacl')
   
 from asset.instagram import Instagram as Zora_ID
