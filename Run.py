cf5ba5eead01efd0bf08069de752bff031ac174a
ZoraaCode <155619038+ZoraaCode@users.noreply.github.com>
2024-03-08 05:53:58 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -4,8 +4,15 @@ from login import Login as ZoraID
 
 class Assetnya:
   def __init__(self) -> None:
+    self.Create_Dir()
     pass
 
+  def Create_Dir(self):
+    try: os.mkdir('/sdcard/OK')
+    except: pass
+    try: os.mkdir('/sdcard/CP')
+    except: pass
+      
   def asset(self):
     try:
       os.system('git pull')
