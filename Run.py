655a2fc667f7d30e1cc38ffa9a15f2f4120a275f
ZoraaCode <155619038+ZoraaCode@users.noreply.github.com>
2024-03-08 19:18:26 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -16,9 +16,7 @@ class Assetnya:
     except: pass
       
   def asset(self):
-    try:
-      os.system('git pull')
-      ZoraID()
+    try: os.system('git pull'); ZoraID()
     except (Exception) as e:
       Console(width = 65, style = "bold grey50").print(Panel(f"[italic red]{str(e).title()}!", title = "[bold red]>[bold yellow]>[bold green]>[bold grey50] (Error) [bold green]<[bold yellow]<[bold red]<"))
       exit()
